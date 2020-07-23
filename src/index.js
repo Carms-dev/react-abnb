1b81fdfbadee616983c753fff275b145280ae760
Carms Ng <carmenhyng89@gmail.com>
2020-07-22 20:16:32 -0400

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import * as serviceWorker from './serviceWorker';
 import './css/style.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
