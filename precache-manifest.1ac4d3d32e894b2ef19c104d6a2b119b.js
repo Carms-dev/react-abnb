e190b9fc3b1c6818c25368be31ec197cd71d8c84
Carms Ng <carmenhyng89@gmail.com>
2020-07-22 20:49:43 -0400

    Updates

---
+++ precache-manifest.1ac4d3d32e894b2ef19c104d6a2b119b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06590d99b0e0f315c969359e0ebfc3a5",
+    "url": "/react-abnb/index.html"
+  },
+  {
+    "revision": "f11fb9d3283eb6da4d63",
+    "url": "/react-abnb/static/css/main.19d18dc7.chunk.css"
+  },
+  {
+    "revision": "23fffd7ae45725f4a75d",
+    "url": "/react-abnb/static/js/2.71572e8a.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/react-abnb/static/js/2.71572e8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f11fb9d3283eb6da4d63",
+    "url": "/react-abnb/static/js/main.272af327.chunk.js"
+  },
+  {
+    "revision": "27bdae43fae08606ff76",
+    "url": "/react-abnb/static/js/runtime-main.1e6dca27.js"
+  }
+]);
